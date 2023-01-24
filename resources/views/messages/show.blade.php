5046d441a589644d3fa52716fc558ca72d5c0073
enzo-radel-signo <enzo.radel@signoweb.com.br>
2023-01-24 15:00:16 -0300

    estilizar telas de edição e visualização das mensagens

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.authenticated_page')
 @section('content')
-    <h1 class="text-xl">Ver detalhes da mensagem</h1>
 
-    <div class="flex flex-wrap mx-5 gap-y-4 gap-x-5">
-        <div class="flex flex-col w-[436px]">
-            <label>Nome</label>
-            <p class="border rounded-lg border-black px-2 py-1 hover:drop-shadow-md focus:drop-shadow-md">{{ $message->name }}</p>
+    <div class="m-10">
+        <div class="">
+            <h1 class="text-2xl font-medium ">Ver detalhes da mensagem</h1>
         </div>
-
-        <div class="flex flex-col w-[436px]">
-            <label>Botão</label>
-            <p class="border rounded-lg border-black px-2 py-1 hover:drop-shadow-md focus:drop-shadow-md">{{ $message->button }}</p>
-        </div>
-
-        <div class="flex flex-col w-full">
-            <label>Conteúdo</label>
-            <p class="border rounded-lg border-black px-2 py-1 hover:drop-shadow-md focus:drop-shadow-md">{{ $message->content }}</p>
-        </div>
-
-        <div>
-            <a href="{{ route("messages.index") }}">
-                <button type="button" class="rounded-full cursor-pointer bg-gray-800 p-2 text-white">Voltar para a listagem</button>
-            </a>
+        <br>
+        <div class="border border-blac p-3 shadow-md bg-gray-50">
+            <div class="flex flex-wrap  gap-y-4 gap-x-5">
+                <div class="flex flex-col w-[436px]">
+                    <label>Nome</label>
+                    <p class="border rounded-lg border-black mt-2 px-2 py-1">{{ $message->name }}</p>
+                </div>
+    
+                <div class="flex flex-col w-[436px]">
+                    <label>Botao</label>
+                    <p class="border rounded-lg border-black mt-2 px-2 py-1">{{ $message->button }}</p>
+                </div>
+    
+                <div class="flex flex-col w-full">
+                    <label for="content">Conteúdo</label>
+                    <p class="border rounded-lg border-black mt-2 px-2 py-1">{{ $message->content }}</p>
+                </div>
+    
+                <div>
+                    <a href="{{ route("messages.index") }}">
+                        <button type="button" class="rounded-full cursor-pointer bg-gray-800 p-2 text-white">Voltar para a listagem</button>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
